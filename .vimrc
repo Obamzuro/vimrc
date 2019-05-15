f0dfb7f2e03aaf20d7b9c8c2c435d5620492e283
root <root@localhost.localdomain>
2019-05-15 03:58:51 -0400

    asm syntastic

--- .vimrc
+++ .vimrc
@@ -31,3 +31,7 @@ let g:syntastic_always_populate_loc_list = 1
 let g:syntastic_auto_loc_list = 1
 let g:syntastic_check_on_open = 1
 let g:syntastic_check_on_wq = 0
+
+" asm helpers for syntastic
+autocmd BufNewFile,BufRead *.asm set filetype=nasm
+" ~/.vim/bundle/syntastic/syntax_checkers/nasm/nasm.vim - add -f elf64 to args for nasm
--- preinstall.sh
+++ preinstall.sh
@@ -2,3 +2,5 @@
 mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 cd ~/.vim/bundle && git clone --depth=1 https://github.com/vim-syntastic/syntastic.git
 git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
+mv ~/.vimrc ~/.vimrc_prev
+mv .vimrc ~/.vimrc
